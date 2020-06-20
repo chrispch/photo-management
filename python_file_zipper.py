a1e1dd361a0604032276c86f1d2dcb3e7c1bf3f7
chris <chonghern@gmail.com>
2020-06-20 23:04:25 +0800

    added multiprocessing to file zipper

--- python_file_zipper.py
+++ python_file_zipper.py
@@ -1,7 +1,14 @@
 import os
 import py7zr
+from time import time
+from datetime import timedelta
+from multiprocessing import Pool
+import math
 
 ACCEPTABLE_EXTENSIONS = ['raf', 'arw', 'jpg', 'jpeg']
+
+_files_processed = 0
+_total = 1
 
 def getFilesFromDir(directory):
     _files = []
@@ -26,25 +33,35 @@ def getTargetDirectory(msg):
         _target = input(msg)
     return _target
 
-def archiveFile(input, output):
+
+def archiveFile(args):
+    input = args[0]
+    output = args[1]
     _archive = py7zr.SevenZipFile(output, 'w')
     _archive.writeall(input)
     _archive.close()
+    # updatePercentage(output)
+
+def updatePercentage(filename):
+    global _files_processed
+    global _total
+
+    _files_processed += 1
+    _percentage_completion = round(_files_processed/_total * 100, 2)
+    print("Zipped {0} ({1}%)".format(filename, _percentage_completion))
 
 def archiveAllIndividually(files, input_dir, output_dir):
+    global _files_processed
+    global _total
     _input_base = os.path.basename(input_dir)
     _total = len(files)
     _files_processed = 0
+    _jobs = [];
     for f in files:
-        _files_processed += 1
-        _percentage_completion = round(_files_processed/_total * 100, 2)
         _input = f
         _rel_path = os.path.relpath(f, input_dir)
         _rel_path = os.path.join(_input_base, _rel_path)
         _output_base = os.path.join(output_dir, _rel_path)
-        # _output_split = _output_with_extension.split('.')
-        # _output_split.pop()
-        # _output_base = ".".join(_output_split) 
         _output = _output_base + ".7z"
         _output_dir = os.path.dirname(_output)
         if not os.path.exists(_output_dir):
@@ -53,26 +70,36 @@ def archiveAllIndividually(files, input_dir, output_dir):
         while os.path.exists(_output):
             _output = _output_base + "_" + str(counter) + ".7z"
             counter += 1
-        print("Zipped {0} ({1}%)".format(_output, _percentage_completion))
-        archiveFile(_input, _output)
+        _jobs.append([_input, _output])
+    
+    print("Using " + str(os.cpu_count()) + " cores.")
+    with Pool() as p:
+        p.map(archiveFile, _jobs)
+
 
 def getConfirmation(target_files):
-  _target_files = target_files
-  for f in _target_files:
-    print(f)
-  _confirmation = input("The above files will be archived. Continue? ({}) (y/n): ".format(len(_target_files)))
-  while not _confirmation in ['y','n']:
-    _confirmation = input("The above files will be archived. Continue? ({}) (y/n): ".format(len(_target_files)))
-  if _confirmation == 'y':
-      return True
-  else:
-    return False
+    _target_files = target_files
+    for f in _target_files:
+        print(f)
+    _confirmation = input(
+        "The above files will be archived. Continue? ({}) (y/n): ".format(len(_target_files)))
+    while not _confirmation in ['y', 'n']:
+        _confirmation = input(
+            "The above files will be archived. Continue? ({}) (y/n): ".format(len(_target_files)))
+    if _confirmation == 'y':
+        return True
+    else:
+        return False
 
-_input_msg = "Enter the root directory to be archived (leave blank to select the current directory): "
-_output_msg = "Enter the target directory for the archived files (leave blank to select the current directory): "
-print(os.path.basename(os.getcwd()))
-_input_dir = getTargetDirectory(_input_msg)
-_input_files = getFilesFromDir(_input_dir)
-_output_dir = getTargetDirectory(_output_msg)
-if (getConfirmation(_input_files)): 
-    archiveAllIndividually(_input_files, _input_dir, _output_dir)
+
+if __name__ == "__main__":
+    currtime = time()
+    _input_msg = "Enter the root directory to be archived (leave blank to select the current directory): "
+    _output_msg = "Enter the target directory for the archived files (leave blank to select the current directory): "
+    _input_dir = getTargetDirectory(_input_msg)
+    _input_files = getFilesFromDir(_input_dir)
+    _output_dir = getTargetDirectory(_output_msg)
+    if (getConfirmation(_input_files)):
+        archiveAllIndividually(_input_files, _input_dir, _output_dir)
+    print("time taken: " + str(timedelta(seconds=time() - currtime)))
+    print("average time per file: " + str((time() - currtime) / _total) + " s")
